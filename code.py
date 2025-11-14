1d2cef6ed9de245e7a7e5b935f3304de887a9792
parsa <aerabi.p@gmail.com>
2025-11-14 12:12:09 +0330

    response defiend

--- code.py
+++ code.py
@@ -1,2 +1,23 @@
-import numpy as np
-0
+user_input = input('please enter your answer: ')
+print(user_input)
+point = 0
+def reward(user_input, correct_answer):
+
+    if user_input == 'r':
+        if correct_answer == 'green':
+            print('Well done')
+            return 1
+        else:
+            print('Try harder')
+            return 0
+
+    if user_input == 'l':
+        if correct_answer == 'blue':
+            print('Well done')
+            return 1
+        else:
+            print('Try harder')
+            return 0
+
+point += reward(user_input, 'green')
+print(point)
