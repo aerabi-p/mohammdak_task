0ac91948ec2bd98fb4cd592fbb9e9509cdbf1060
Yasaman <yasaman.babajani@yahoo.com>
2025-11-14 12:29:44 +0330

    fix #1

--- code.py
+++ code.py
@@ -1,2 +1,11 @@
 import numpy as np
-ye chizi
+def correct_answer_generator():
+    prob=np.random.uniform()
+    if prob <= 0.7:
+        correctanswer="blue"
+    else:
+        correctanswer="green"
+
+    return correctanswer
+
+print(correct_answer_generator())
