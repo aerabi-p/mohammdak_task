e17c64ebebda7cbfe2c893e61436f6fadb6acfc3
parsa <aerabi.p@gmail.com>
2025-11-14 18:25:38 +0330

    take care

--- code.py
+++ code.py
@@ -3,31 +3,25 @@ import numpy as np
 def correct_answer_generator():
     prob=np.random.uniform()
     if prob <= 0.7:
-        correctanswer="blue"
+        correct_answer="blue"
     else:
-        correctanswer="green"
+        correct_answer="green"
 
-    return correctanswer
+    return correct_answer
 
 def gazing(correct_answer):
     
     prob = np.random.uniform()
     print(prob)
     if prob<=0.7:
-        print(correct_answer)
         return correct_answer
     else:
         if correct_answer=="green":
-            print("blue")
             return "blue"
         else:
-            print("green")
             return "green"
-user_input = input('please enter your answer: ')
-print(user_input)
-point = 0
+        
 def reward(user_input, correct_answer):
-
     if user_input == 'r':
         if correct_answer == 'green':
             print('Well done')
@@ -43,6 +37,26 @@ def reward(user_input, correct_answer):
         else:
             print('Try harder')
             return 0
+import pandas as pd
+point = 0
+count = 0
+data = []
+for i in range(5) :     
+    count +=1  
+    user_input = input('please enter your answer: ')
+    print(user_input)
+    correct_answer = correct_answer_generator()
+    gaze = gazing(correct_answer)
+    print(gaze)
+    point += reward(user_input, correct_answer)
+    print(point)
+    data.append([count, gaze,user_input, correct_answer, point])
+print(data)
+columns=["trial", "gaze_direction", "chosen_card", "correct_card", "reward"]
+data_csv = pd.DataFrame(data, columns=columns)
+print(data_csv)
+data_csv.to_csv()
 
-point += reward(user_input, 'green')
-print(point)
+
+
+ 
