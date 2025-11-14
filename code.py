39d9b93ad04ed0264dc47265a3a736cb5babf8cf
ZeinabL0 <l0tfi.zeinab21@gmail.com>
2025-11-14 11:34:19 +0330

    gazing function

--- code.py
+++ code.py
@@ -1,2 +1,15 @@
 import numpy as np
-print("none")
+def gazing(correct_answer):
+    
+    prob = np.random.uniform()
+    print(prob)
+    if prob<=0.7:
+        print(correct_answer)
+    else:
+        if correct_answer=="green":
+            print("blue")
+        else:
+            print("green")
+    
+        
+gazing("blue")
